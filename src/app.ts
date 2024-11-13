542e1ec9ce542a9f5426c2d10a4d482ddbb4b93c
mzfirozuddin <mz.firozuddin@gmail.com>
2024-11-13 23:38:25 +0530

    add some common middleware

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
 import express from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { config } from "./config/config";
 
 const app = express();
+
+//:Middleware for handle CORS error
+app.use(cors({ origin: config.cors_origin, credentials: true }));
+//: Middleware for handle json data
+app.use(express.json());
+//: Middleware for handle data from url
+app.use(express.urlencoded({ extended: true }));
+//: Middleware for handle cookie efficiently
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
     res.send("Hello from home!!!");
--- src/config/config.ts
+++ src/config/config.ts
@@ -5,6 +5,7 @@ const _config = {
     port: process.env.PORT,
     db_url: process.env.MONGODB_URL,
     env: process.env.NODE_ENV,
+    cors_origin: process.env.CORS_ORIGIN,
 };
 
 export const config = Object.freeze(_config);
