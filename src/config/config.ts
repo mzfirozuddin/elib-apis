6262ae0545a01118e49940d981e1a5422669f593
mzfirozuddin <mz.firozuddin@gmail.com>
2024-11-18 00:11:17 +0530

    add token service module

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,6 +6,10 @@ const _config = {
     db_url: process.env.MONGODB_URL,
     env: process.env.NODE_ENV,
     cors_origin: process.env.CORS_ORIGIN,
+    accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
+    accessTokenExpiry: process.env.ACCESS_TOKEN_EXPIRY,
+    refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET,
+    refreshTokenExpiry: process.env.REFRESH_TOKEN_EXPIRY,
 };
 
 export const config = Object.freeze(_config);
---
+++ src/services/tokenService.ts
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import { config } from "../config/config";
+import { IPayload } from "./types";
+
+class TokenService {
+    generateAccessToken(payload: IPayload) {
+        return jwt.sign(
+            { sub: payload.id, email: payload.email, name: payload.name },
+            config.accessTokenSecret as string,
+            { expiresIn: config.accessTokenExpiry, algorithm: "HS256" }
+        );
+    }
+
+    generateRefreshToken(id: string) {
+        return jwt.sign({ sub: id }, config.refreshTokenSecret as string, {
+            expiresIn: config.refreshTokenExpiry,
+            algorithm: "HS256",
+        });
+    }
+}
+
+export default TokenService;
