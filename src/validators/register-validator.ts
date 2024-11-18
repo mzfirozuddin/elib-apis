869825531b2210b6ec4ac8a935dc24a8604379a5
mzfirozuddin <mz.firozuddin@gmail.com>
2024-11-19 01:00:19 +0530

    add register validator

---
+++ src/validators/register-validator.ts
@@ -0,0 +1,42 @@
+import { checkSchema } from "express-validator";
+
+export default checkSchema({
+    name: {
+        errorMessage: "Name is required!",
+        notEmpty: true,
+        trim: true,
+    },
+
+    email: {
+        errorMessage: "Email is required!",
+        notEmpty: true,
+        trim: true,
+        isEmail: {
+            errorMessage: "Email must be a valid email address!",
+        },
+    },
+
+    password: {
+        errorMessage: "Password is required!",
+        trim: true,
+        notEmpty: true,
+        isLength: {
+            options: { min: 8 },
+            errorMessage: "Password length should be at least 8 chars!",
+        },
+    },
+
+    confirmPassword: {
+        custom: {
+            options: (value, { req }) => {
+                if (value !== req.body.password) {
+                    throw new Error(
+                        "Password and ConfirmPassword are not matched!"
+                    );
+                }
+
+                return true;
+            },
+        },
+    },
+});
