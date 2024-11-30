6410495857ba5e2789a48552ed4031a5e0856b03
mzfirozuddin <mz.firozuddin@gmail.com>
2024-11-30 23:25:47 +0530

    add auth middleware

---
+++ src/middlewares/AuthMiddleware.ts
@@ -0,0 +1,46 @@
+import { NextFunction, Response } from "express";
+import createHttpError from "http-errors";
+import jwt from "jsonwebtoken";
+import { config } from "../config/config";
+import { User } from "../user/user.model";
+import { CustomRequest } from "../services/types";
+
+export const verifyJWT = async (
+    req: CustomRequest,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        //: Collect Token
+        const token =
+            req.cookies?.accessToken ||
+            req.header("Authorization")?.replace("Bearer ", "");
+
+        if (!token) {
+            const err = createHttpError(401, "Unauthorized Request!");
+            return next(err);
+        }
+
+        //: verify token
+        const decodedToken = jwt.verify(
+            token,
+            config.accessTokenSecret as string
+        );
+
+        //: check for user
+        const user = await User.findById(decodedToken?.sub).select(
+            "-password -refreshToken"
+        );
+        if (!user) {
+            const err = createHttpError(401, "Inavlid access token!");
+            return next(err);
+        }
+
+        //: add the user in req object
+        req.user = user;
+        next();
+    } catch (err) {
+        next(err);
+        return;
+    }
+};
--- src/services/types.ts
+++ src/services/types.ts
@@ -1,5 +1,20 @@
+import { Request } from "express";
+
 export interface IPayload {
     id: string;
     email: string;
     name: string;
+}
+
+export interface ICurrentUser {
+    _id: string;
+    name: string;
+    email: string;
+    avatar: string;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface CustomRequest extends Request {
+    user?: ICurrentUser;
 }
